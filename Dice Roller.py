a025747ae0be8aef8ef200829ea111495166feab
AddisonPascal <addisondj@hotmail.com>
2017-11-24 18:55:26 +1100

    Commented
    Commented the program.

--- Dice Roller.py
+++ Dice Roller.py
@@ -1,10 +1,23 @@
+## Version 1.0.1
+## By Addison Djatschenko
+
+##########################################################
+
+## Allows random numbers to be used in the file
 import random
+## Allows time in between rolls
 import time
+## User inputs
 times = int( input('Amount of rolls: '))
 sides = int( input('Amount of sides: '))
 wait = float( input('Seconds in between each roll: '))
+## Sets the dice rolls so far to zero
 timesSoFar = int( 0)
+## Runs until timesSoFar equals times
 while timesSoFar != times:
+    ## Displays current dice roll
     print (random.randint(1, sides))
+    ## Increments timesSoFar
     timesSoFar = timesSoFar+1
+    ## Waits for wait seconds
     time.sleep(wait)
